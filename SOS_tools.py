a45481fae5915efc0a4c1aec38055737e92ed1ce
Djalel Benbouzid <djalel.benbouzid@gmail.com>
2014-05-27 18:31:18 +0200

    no message

--- SOS_tools.py
+++ SOS_tools.py
@@ -4,6 +4,9 @@ import pylab as plt
 
 from collections import namedtuple
 
+from sklearn.utils import check_arrays, column_or_1d
+from sklearn.utils.multiclass import type_of_target
+
 class HiggsData:
     
     def __init__(self, file_name, is_test_data=None):
@@ -112,7 +115,7 @@ class HiggsData:
         if self.is_test_data:
             return self.data.label_idx
 
-    # Splitting the dataset into train and valid
+    # Splitting the dataset 
     def _compute_random_indices(self):
         self.random_indices = np.random.permutation(self.data.index)
         # self.random_indices = np.random.choice(self.data.index, int(self.fraction * self.num_instances), replace=False)
@@ -133,13 +136,13 @@ class HiggsData:
         try:
             iter(fractions)
         except TypeError:
-            fractions = (fractions,)
+            fractions = (fractions, 0)
 
         self.split_fractions = fractions
         self._compute_random_indices()
 
 
-    def get_data_fold(self, fold,normed_weights=True, number=None):
+    def get_data_fold(self, fold, number=None, normed_weights=True):
         """
         Returns a named tuple (X, Y, Weights) 
         after computing the new weights
@@ -153,9 +156,20 @@ class HiggsData:
         # W = data[['Weight']] # force the creation of a compy 
         # Y = data.label_idx # return a view, not copies
 
-        indices = self.random_indices
         if fold == 'train':
-            indices = self.data.index.drop(indices)
+            start_split = 0
+            end_split = int(self.data.shape[0] * self.split_fractions[0])
+        elif fold == 'valid':
+            start_split = int(self.data.shape[0] * self.split_fractions[0])
+            end_split = int(self.data.shape[0] * sum(self.split_fractions))
+        elif fold == 'test':
+            start_split = int(self.data.shape[0] * sum(self.split_fractions))
+            end_split = self.data.shape[0] + 1
+
+        indices = self.random_indices[start_split:end_split]
+
+        if number != None and number < len(indices): 
+            indices = indices[:number]
 
         X = self.data[self.data_columns].ix[indices]
         W = self.data.Weight.ix[indices]
@@ -166,42 +180,24 @@ class HiggsData:
             for l_idx in (0, 1):
                 W[Y == l_idx] = 0.5 * W / W[Y == l_idx].sum()
 
-        return namedtuple('Return', 'X Y Weights')(X, Y, W)
-
-    def get_subset(self, number, clear=False, noise=None):
-
-        if clear: 
-            self.remaining_index = self.data.index
-        indices = np.random.choice(self.remaining_index, number, replace=False)
-        self.remaining_index.drop(indices)
-
-        X = self.data[self.data_columns].ix[indices] 
-        # X += np.random.normal(0., 0.01, X.shape)
-        W = self.data.Weight.ix[indices]
-        Y = self.data.label_idx.ix[indices]
-
-        if noise:
-            X += np.random.normal(0., X.replace(-999., np.nan).std() * noise, X.shape)
-
-        return namedtuple('Return', 'X Y Weights')(X, Y, W)
-        return self.data.ix[np.random.choice(self.data.index, number, replace=False)]
-
+        return namedtuple('Return', 'X Y Weights')(X.values, Y.values, W.values)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def step_wise_performance(predictor, X, Y, weights=None, metric=None):
     assert len(X) == len(Y)
 
+    if hasattr(weights, 'values'): weights = weights.values
+
     num_iterations = predictor.n_estimators
     if metric == None:
-        from sklearn.metrics import zero_one_loss
         metric = zero_one_loss
 
     stage_wise_perf = np.zeros(num_iterations)
     if hasattr(predictor, 'staged_predict'):
         # stage_wise_perf = list(predictor.staged_score(X, Y))
         for i, pred in enumerate(predictor.staged_predict(X)):
-            stage_wise_perf[i] = metric(Y, pred) #, sample_weight=weights
+            stage_wise_perf[i] = metric(Y, pred, sample_weight=weights)
 
     else:
         prediction = np.zeros(X.shape[0])
@@ -213,7 +209,13 @@ def step_wise_performance(predictor, X, Y, weights=None, metric=None):
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def learning_curve_plot(predictors, train_X, train_Y, valid_X, valid_Y, weights=None, with_train=True, log_scale=False):
+def learning_curve_plot(predictors, valid_X, valid_Y, valid_W=None, train_X=None, train_Y=None, train_W=None, log_scale=False):
+
+    if hasattr(valid_W, 'values'): valid_W = valid_W.values
+    if train_W != None and hasattr(train_W, 'values'): train_W = weights.values
+
+    if train_X != None and valid_X != None: with_train = True
+
     try:
         predictors = predictors.items()
     except:
@@ -221,10 +223,10 @@ def learning_curve_plot(predictors, train_X, train_Y, valid_X, valid_Y, weights=
 
     for name, predictor in predictors:
         iterations = np.arange(1, predictor.n_estimators + 1)
-        p, = plt.plot(iterations, step_wise_performance(predictor, valid_X, valid_Y, weights), '-', label=name + ' (test)')
+        p, = plt.plot(iterations, step_wise_performance(predictor, valid_X, valid_Y, valid_W), '-', label=name + ' (test)')
 
         if with_train:
-            plt.plot(iterations, step_wise_performance(predictor, train_X, train_Y, weights), '--', color=p.get_color(), label=name + ' (train)')
+            plt.plot(iterations, step_wise_performance(predictor, train_X, train_Y, train_W), '--', color=p.get_color(), label=name + ' (train)')
 
         plt.legend(loc='best')
 
@@ -243,15 +245,17 @@ def _AMS(s, b, b_regul = 10.):
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def plot_AMS(scores, Y, W, weight_factor, current_ax=None):
+def plot_AMS(scores, labels, weights, weight_factor, **kwargs):
 
-    if current_ax == None:
-        current_ax = plt.figure().add_subplot(111)
+    if hasattr(scores, 'values'): scores = scores.values
+    if hasattr(labels, 'values'): labels = labels.values
+    if hasattr(weights, 'values'): weights = weights.values
 
     sorted_indices = scores[:,1].argsort()
-    signal_weight_sum = W[Y == 1].sum()
-    bkgd_weight_sum = W[Y == 0].sum()
+    signal_weight_sum = weights[labels == 1].sum()
+    bkgd_weight_sum = weights[labels == 0].sum()
 
+    print signal_weight_sum
     ams = np.zeros(sorted_indices.shape[0])
     max_ams = 0
     threshold = -1
@@ -270,18 +274,24 @@ def plot_AMS(scores, Y, W, weight_factor, current_ax=None):
             max_ams = ams[i]
             threshold = i
 
-        if Y[current_instance] == 1:
-            signal_weight_sum -= W[current_instance]
+        if labels[current_instance] == 1:
+            signal_weight_sum -= weights[current_instance]
         else:
-            bkgd_weight_sum -= W[current_instance]
+            bkgd_weight_sum -= weights[current_instance]
 
-    current_ax.plot(ams)
+    plt.plot(ams, **kwargs)
+    if 'label' in kwargs: plt.legend(loc='best')
     # current_ax.xlim(0, len(sorted_indices)-1)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 def hist_scores(scores, labels, weights=None, **kwargs):
+
+    if hasattr(scores, 'values'): scores = scores.values
+    if hasattr(labels, 'values'): labels = labels.values
+    if hasattr(weights, 'values'): weights = weights.values
+
     if weights != None:
         s_w = weights[labels==1]
         b_w = weights[labels==0]
@@ -292,16 +302,21 @@ def hist_scores(scores, labels, weights=None, **kwargs):
     kwargs['normed'] = kwargs.get('normed', True)
     kwargs['histtype'] = kwargs.get('histtype', 'stepfilled')
 
-    plt.hist(scores[:,1][labels==1], alpha=.5, label='signal', weights=s_w, color='blue', **kwargs)
-    plt.hist(scores[:,1][labels==0], alpha=.5, label='bkgd', weights=b_w, color='red', **kwargs)
-    plt.legend(loc='best')
-#     gca().set_yscale('log')
-#     print gca().get_yscale()
-#     plt.semilogy()
+    if scores.ndim == 2: 
+        scores = scores[:,1]
 
+    plt.hist(scores[labels==1], alpha=.5, label='signal', weights=s_w, color='blue', **kwargs)
+    plt.hist(scores[labels==0], alpha=.5, label='bkgd', weights=b_w, color='red', **kwargs)
+    plt.legend(loc='best')
+
+    # plt.xlim((0., 1.))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def hist_scores_log(scores, labels, weights=None, ln=True, plot_ams=False, **kwargs): 
+def hist_scores_log(scores, labels, weights=None, ln=True, **kwargs): 
+
+    if hasattr(scores, 'values'): scores = scores.values
+    if hasattr(labels, 'values'): labels = labels.values
+    if hasattr(weights, 'values'): weights = weights.values
 
     if weights != None:
         s_w = weights[labels==1]
@@ -312,66 +327,33 @@ def hist_scores_log(scores, labels, weights=None, ln=True, plot_ams=False, **kwa
     kwargs['bins'] = kwargs.get('bins', 100)
     kwargs['normed'] = kwargs.get('normed', True)
 
-    width = 1./ kwargs.get('bins', 50)
+    # weight_factor = None
+    # if 'weight_factor' in kwargs:
+    #     weight_factor = kwargs['weight_factor']
+    #     del kwargs['weight_factor']
 
-    weight_factor = None
-    if 'weight_factor' in kwargs:
-        weight_factor = kwargs['weight_factor']
-        del kwargs['weight_factor']
-
-    hist_s, bins_s = np.histogram(scores[:,1][labels==1], weights=s_w, **kwargs)
-    hist_b, bins_b = np.histogram(scores[:,1][labels==0], weights=b_w, **kwargs)
+    if scores.ndim == 2: 
+        scores = scores[:,1]
+    hist_s, bins_s = np.histogram(scores[labels==1], weights=s_w, **kwargs)
+    hist_b, bins_b = np.histogram(scores[labels==0], weights=b_w, **kwargs)
 
     if ln:
-        hist_s = np.log(hist_s) 
-        hist_b = np.log(hist_b) 
-        hist_min = min(hist_s.min(), hist_b.min())
-        hist_s -= hist_min 
-        hist_b -= hist_min 
+        nz_s = hist_s.nonzero()[0]
+        hist_s[nz_s] = np.log(hist_s[nz_s])
+        nz_b = hist_b.nonzero()[0]
+        hist_b[nz_b] = np.log(hist_b[nz_b])
+        hist_min = min(hist_s[nz_s].min(), hist_b[nz_b].min())
+        hist_s[nz_s] -= hist_min 
+        hist_b[nz_b] -= hist_min 
 
+
+    width =  0.99 * (max(bins_s.max(), bins_b.max()) - min(bins_s.min(), bins_b.min()))  / kwargs['bins'] 
+    
     plt.bar(bins_s[:-1], hist_s, width=width, color='blue', alpha=.5, label='signal')
     plt.bar(bins_b[:-1], hist_b, width=width, color='red', alpha=.5, label='bkgd')
 
     plt.legend(loc='best')
-
-    if plot_ams:
-
-        if weight_factor == None:
-            print '[!] Please specify the weight_factor in the parameters list.'
-            return 
-
-        # plot_AMS(scores, labels, weights, 2., current_ax=ax)
-        sorted_indices = scores[:,1].argsort()
-        signal_weight_sum = weights[labels == 1].sum()
-        bkgd_weight_sum = weights[labels == 0].sum()
-
-
-        ams = np.zeros(sorted_indices.shape[0])
-        max_ams = 0
-        threshold = -1
-
-        for i, current_instance in enumerate(sorted_indices):
-            try:
-                ams[i] = _AMS(signal_weight_sum * weight_factor, bkgd_weight_sum * weight_factor)
-            except:
-                # tmp code for debugging the numerical error
-                print i, '/', len(sorted_indices), '|', current_instance
-                print signal_weight_sum
-                print bkgd_weight_sum
-                raise
-
-            if ams[i] > max_ams:
-                max_ams = ams[i]
-                threshold = i
-
-            if labels[current_instance] == 1:
-                signal_weight_sum -= weights[current_instance]
-            else:
-                bkgd_weight_sum -= weights[current_instance]
-
-        plt.plot(ams)
-        # plt.xlim(0, len(sorted_indices)-1)
-    
+    # plt.xlim((0., 1.))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -390,7 +372,223 @@ def load_model(file_name):
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def zero_one_loss(y_true, y_pred, normalize=True, sample_weight=None):
+    """Zero-one classification loss.
 
+If normalize is ``True``, return the fraction of misclassifications
+(float), else it returns the number of misclassifications (int). The best
+performance is 0.
+
+Parameters
+----------
+y_true : array-like or list of labels or label indicator matrix
+Ground truth (correct) labels.
+
+y_pred : array-like or list of labels or label indicator matrix
+Predicted labels, as returned by a classifier.
+
+normalize : bool, optional (default=True)
+If ``False``, return the number of misclassifications.
+Otherwise, return the fraction of misclassifications.
+
+sample_weight : array-like of shape = [n_samples], optional
+Sample weights.
+
+Returns
+-------
+loss : float or int,
+If ``normalize == True``, return the fraction of misclassifications
+(float), else it returns the number of misclassifications (int).
+
+Notes
+-----
+In multilabel classification, the zero_one_loss function corresponds to
+the subset zero-one loss: for each sample, the entire set of labels must be
+correctly predicted, otherwise the loss for that sample is equal to one.
+
+See also
+--------
+accuracy_score, hamming_loss, jaccard_similarity_score
+
+Examples
+--------
+>>> from sklearn.metrics import zero_one_loss
+>>> y_pred = [1, 2, 3, 4]
+>>> y_true = [2, 2, 3, 4]
+>>> zero_one_loss(y_true, y_pred)
+0.25
+>>> zero_one_loss(y_true, y_pred, normalize=False)
+1
+
+In the multilabel case with binary indicator format:
+
+>>> zero_one_loss(np.array([[0.0, 1.0], [1.0, 1.0]]), np.ones((2, 2)))
+0.5
+
+and with a list of labels format:
+
+>>> zero_one_loss([(1, ), (3, )], [(1, 2), tuple()])
+1.0
+
+
+"""
+    score = accuracy_score(y_true, y_pred,
+                           normalize=normalize,
+                           sample_weight=sample_weight)
+
+    if normalize:
+        return 1 - score
+    else:
+        if sample_weight is not None:
+            n_samples = np.sum(sample_weight)
+        else:
+            n_samples = len(y_true)
+        return n_samples - score
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):
+    """Accuracy classification score.
+
+    In multilabel classification, this function computes subset accuracy:
+    the set of labels predicted for a sample must *exactly* match the
+    corresponding set of labels in y_true.
+
+    Parameters
+    ----------
+    y_true : array-like or list of labels or label indicator matrix
+    Ground truth (correct) labels.
+
+    y_pred : array-like or list of labels or label indicator matrix
+    Predicted labels, as returned by a classifier.
+
+    normalize : bool, optional (default=True)
+    If ``False``, return the number of correctly classified samples.
+    Otherwise, return the fraction of correctly classified samples.
+
+    sample_weight : array-like of shape = [n_samples], optional
+    Sample weights.
+
+    Returns
+    -------
+    score : float
+    If ``normalize == True``, return the correctly classified samples
+    (float), else it returns the number of correctly classified samples
+    (int).
+
+    The best performance is 1 with ``normalize == True`` and the number
+    of samples with ``normalize == False``.
+
+    See also
+    --------
+    jaccard_similarity_score, hamming_loss, zero_one_loss
+
+    Notes
+    -----
+    In binary and multiclass classification, this function is equal
+    to the ``jaccard_similarity_score`` function.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.metrics import accuracy_score
+    >>> y_pred = [0, 2, 1, 3]
+    >>> y_true = [0, 1, 2, 3]
+    >>> accuracy_score(y_true, y_pred)
+    0.5
+    >>> accuracy_score(y_true, y_pred, normalize=False)
+    2
+
+    In the multilabel case with binary indicator format:
+
+    >>> accuracy_score(np.array([[0.0, 1.0], [1.0, 1.0]]), np.ones((2, 2)))
+    0.5
+
+    and with a list of labels format:
+
+    >>> accuracy_score([(1, ), (3, )], [(1, 2), tuple()])
+    0.0
+
+    """
+
+    # Compute accuracy for each possible representation
+    y_type, y_true, y_pred = _check_clf_targets(y_true, y_pred)
+    if y_type == 'multilabel-indicator':
+        score = (y_pred != y_true).sum(axis=1) == 0
+    elif y_type == 'multilabel-sequences':
+        score = np.array([len(set(true) ^ set(pred)) == 0
+                          for pred, true in zip(y_pred, y_true)])
+    else:
+        score = y_true == y_pred
+
+    if normalize:
+        if sample_weight is not None:
+            return np.average(score, weights=sample_weight)
+        return np.mean(score)
+    else:
+        if sample_weight is not None:
+            return np.dot(score, sample_weight)
+        return np.sum(score)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def _check_clf_targets(y_true, y_pred):
+    """Check that y_true and y_pred belong to the same classification task
+
+    This converts multiclass or binary types to a common shape, and raises a
+    ValueError for a mix of multilabel and multiclass targets, a mix of
+    multilabel formats, for the presence of continuous-valued or multioutput
+    targets, or for targets of different lengths.
+
+    Column vectors are squeezed to 1d.
+
+    Parameters
+    ----------
+    y_true : array-like,
+
+    y_pred : array-like
+
+    Returns
+    -------
+    type_true : one of {'multilabel-indicator', 'multilabel-sequences', \
+    'multiclass', 'binary'}
+    The type of the true target data, as output by
+    ``utils.multiclass.type_of_target``
+
+    y_true : array or indicator matrix or sequence of sequences
+
+    y_pred : array or indicator matrix or sequence of sequences
+    """
+
+    y_true, y_pred = check_arrays(y_true, y_pred, allow_lists=True)
+    type_true = type_of_target(y_true)
+    type_pred = type_of_target(y_pred)
+
+    y_type = set([type_true, type_pred])
+    if y_type == set(["binary", "multiclass"]):
+        y_type = set(["multiclass"])
+
+    if len(y_type) > 1:
+        raise ValueError("Can't handle mix of {0} and {1}"
+                         "".format(type_true, type_pred))
+
+    # We can't have more than one value on y_type => The set is no more needed
+    y_type = y_type.pop()
+
+    # No metrics support "multiclass-multioutput" format
+    if (y_type not in ["binary", "multiclass", "multilabel-indicator",
+                       "multilabel-sequences"]):
+        raise ValueError("{0} is not supported".format(y_type))
+
+    if y_type in ["binary", "multiclass"]:
+        y_true = column_or_1d(y_true)
+        y_pred = column_or_1d(y_pred)
+
+    return y_type, y_true, y_pred
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# output with weights 
 
 # %load_ext hierarchymagic
 # with open("output.dot", "w") as output_file:
